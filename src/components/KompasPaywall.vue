f968b495f337d5bd427218c768347a59e7effb6f
Deny Ramanda Liu <ramandadeny7@gmail.com>
2025-06-26 15:47:58 +0700

    Add redirects

--- src/components/KompasPaywall.vue
+++ src/components/KompasPaywall.vue
@@ -3,7 +3,7 @@ const props = defineProps<{ msg?: string }>();
 </script>
 
 <template>
-  <div class="p-4 rounded-lg bg-blue-600 text-white">
+  <div class="p-4 rounded-lg bg-blue-600 text-white bg-black">
     <strong>SDK widget</strong><br />
     <slot>{{ props.msg }}</slot>
   </div>
